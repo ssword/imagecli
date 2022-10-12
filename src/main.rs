408f085e59e5c4a588647b3dc6d6d424bb618a6b
ssword <ssword@gmail.com>
2022-10-13 00:00:20 +0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use std::thread::sleep;
+use std::time::{Duration, Instant};
+
+fn main() {
+    let now = Instant::now();
+    sleep(Duration::new(3, 0));
+    println!("{:?}", now.elapsed().as_secs());
+}
